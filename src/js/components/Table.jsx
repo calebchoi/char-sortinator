230e9e83df7bb1f7e0735f89e30aae306f5b30b7
Caleb Choi <calebc1205@gmail.com>
2018-01-22 16:07:43 -0800

    fixed spacing and layout

--- src/js/components/Table.jsx
+++ src/js/components/Table.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
 const Table = (props) => {
-  return (<div>
-    <h4> Submitted, Sorted </h4>
-    <ol>
-    {props.submitted.map(pair => 
-      <div>
-        <li>
-          <div className="submitted">{pair[0]}</div>
-          <div className="sorted">{pair[1]}</div>
-        </li>
-      </div>
-    )}
-    </ol>
+  return (<div className="table">
+    <div className="submitted">
+      <h4> Submitted </h4>
+      <ol>
+      {props.submitted.map(pair => 
+        <div className="bullet">
+          <li>
+            <div>{pair[0]}</div>
+          </li>
+        </div>
+      )}
+      </ol>
+    </div>
+    <div className="sorted">
+      <h4> Reverse Sorted </h4>
+      <ol>
+      {props.submitted.map(pair => 
+        <div className="bullet">
+          <li>
+            <div>{pair[1]}</div>
+          </li>
+        </div>
+      )}
+      </ol>
+    </div>
   </div>)
 }
 
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      submitted: [['hello', 'ollhe']]
+      submitted: []
     }
 
     this.handleNewSubmission = this.handleNewSubmission.bind(this);
